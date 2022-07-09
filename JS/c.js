2d2ffea44550f3f39a9eb7ac283c244d268e757f
Yatin1802 <leapfrog.testseries@gmail.com>
2022-07-09 08:58:46 +0530

    Update c.js

--- JS/c.js
+++ JS/c.js
@@ -8,12 +8,26 @@ var Global_C =[
     ['yatin5','yatin15',2],
     ['yatin6','yatin16',1],
     ['yatin7','yatin17',0],
-    ['trial','trial7',0],
     ['archa','arch9755',1],
     ['yatin','yat105110',1],
     ['trial77','aobr125152',1],
-    ['kanika96','kanika5750',1]
 
+    ['kanika96','kanika5750',1],
+    ['harpreet98','harpreet5752',1],
+    ['anamika95','anamika5652',1],
+    ['rao14','rao5154',1],
+    ['vineet94','vineet5755',1],
+    ['rao86','rao4950',1],
+    ['priyanka81','priyanka4954',1],
+    ['lekhraj90','lekhraj5753',1],
+    ['praveen87','praveen5755',1],
+    ['khatri42','khatri5151',1],
+
+
+
+
+
+    ['trial','trial7',0]
     
 
 ];
